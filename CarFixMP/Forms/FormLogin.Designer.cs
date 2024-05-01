9f6c32b34dcaf79bc7cba9783b4b2c7533cb995d
xDeserek_ <dawiddominik@interia.eu>
2024-05-01 12:57:40 +0200

    Added Login Form

---
+++ CarFixMP/Forms/FormLogin.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace CarFix.Forms
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.lblCreateAccount = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.CheckBoxShowLoginPassword = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PictureQuitLogin = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureQuitLogin)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCreateAccount
+            // 
+            this.lblCreateAccount.AutoSize = true;
+            this.lblCreateAccount.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblCreateAccount.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblCreateAccount.ForeColor = System.Drawing.Color.Black;
+            this.lblCreateAccount.Location = new System.Drawing.Point(105, 435);
+            this.lblCreateAccount.Name = "lblCreateAccount";
+            this.lblCreateAccount.Size = new System.Drawing.Size(104, 16);
+            this.lblCreateAccount.TabIndex = 22;
+            this.lblCreateAccount.Text = "Create Account";
+            this.lblCreateAccount.Click += new System.EventHandler(this.lblCreateAccount_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.Color.DimGray;
+            this.label5.Location = new System.Drawing.Point(94, 410);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(142, 16);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Dont Have an Account?";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Black;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnLogin.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(38, 326);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(232, 39);
+            this.btnLogin.TabIndex = 20;
+            this.btnLogin.Text = "LOGIN";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // CheckBoxShowLoginPassword
+            // 
+            this.CheckBoxShowLoginPassword.AutoSize = true;
+            this.CheckBoxShowLoginPassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CheckBoxShowLoginPassword.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.CheckBoxShowLoginPassword.ForeColor = System.Drawing.Color.DimGray;
+            this.CheckBoxShowLoginPassword.Location = new System.Drawing.Point(154, 271);
+            this.CheckBoxShowLoginPassword.Name = "CheckBoxShowLoginPassword";
+            this.CheckBoxShowLoginPassword.Size = new System.Drawing.Size(116, 20);
+            this.CheckBoxShowLoginPassword.TabIndex = 19;
+            this.CheckBoxShowLoginPassword.Text = "Show Password";
+            this.CheckBoxShowLoginPassword.UseVisualStyleBackColor = true;
+            this.CheckBoxShowLoginPassword.CheckedChanged += new System.EventHandler(this.CheckBoxShowLoginPassowrd_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.DimGray;
+            this.label3.Location = new System.Drawing.Point(35, 211);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.Color.DimGray;
+            this.label2.Location = new System.Drawing.Point(35, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Username";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtPassword.Location = new System.Drawing.Point(38, 230);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(232, 26);
+            this.txtPassword.TabIndex = 14;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtUsername.Location = new System.Drawing.Point(38, 148);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(232, 26);
+            this.txtUsername.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(45, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 37);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Get Started";
+            // 
+            // PictureQuitLogin
+            // 
+            this.PictureQuitLogin.BackColor = System.Drawing.Color.Transparent;
+            this.PictureQuitLogin.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PictureQuitLogin.BackgroundImage")));
+            this.PictureQuitLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.PictureQuitLogin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PictureQuitLogin.Location = new System.Drawing.Point(12, 12);
+            this.PictureQuitLogin.Name = "PictureQuitLogin";
+            this.PictureQuitLogin.Size = new System.Drawing.Size(35, 35);
+            this.PictureQuitLogin.TabIndex = 23;
+            this.PictureQuitLogin.TabStop = false;
+            this.PictureQuitLogin.Click += new System.EventHandler(this.PictureQuit_Click);
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(300, 500);
+            this.Controls.Add(this.PictureQuitLogin);
+            this.Controls.Add(this.lblCreateAccount);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.CheckBoxShowLoginPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.DimGray;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureQuitLogin)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCreateAccount;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.CheckBox CheckBoxShowLoginPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox PictureQuitLogin;
+    }
+}
---
+++ CarFixMP/Forms/FormLogin.cs
@@ -0,0 +1,66 @@
+﻿using CarFix.CustomControls;
+using CarFix.Data;
+using CarFixMP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarFix.Forms
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            var con = DbRegister.GetConnection();
+            string login = "SELECT * FROM users_table WHERE username= '" + txtUsername.Text + "' AND password= '" + txtPassword.Text + "'";
+            SqlCommand cmd = new SqlCommand(login, con);
+
+            if (txtUsername.Text == "" && txtPassword.Text == "")
+            {
+                MessageBox.Show("Username and Password fields are empty", "Login failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                cmd.ExecuteNonQuery();
+                con.Close();
+                new CarFixInfo().Show();
+   this.Hide();
+            }
+         
+
+        }
+
+        private void lblCreateAccount_Click(object sender, EventArgs e)
+        {
+            new FormRegister().Show();
+            this.Hide();
+        }
+
+        private void CheckBoxShowLoginPassowrd_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CheckBoxShowLoginPassword.Checked)
+            {
+                txtPassword.PasswordChar = '\0';
+            }
+            else
+            {
+                txtPassword.PasswordChar = '*';
+            }
+        }
+
+        private void PictureQuit_Click(object sender, EventArgs e) => CustomButtons.Exit() ;
+
+    }
+}
